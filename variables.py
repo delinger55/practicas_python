555a881a0242e711a477e05f3c5df32bad9ca528
Enzo Rojas <enzorojas21@gmail.com>
2024-07-31 10:51:04 -0400

    Agregando variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+#variables de tipo entero
+a = 10
+b = 20
+print (a)
+print (b)
+
+#Variables flotantes
+c = 1.5
+d = 3.7
+suma = c + d
+print(suma)
+
+#Variable de tipo string
+nombre = "Roberto"
+apellido = "Hernandez"
+fullname = nombre + " " + apellido
+print(fullname)
+
+#Variable de tipo boolean
+lampara = True
+print(lampara)
+lampara = False
+print(lampara)
